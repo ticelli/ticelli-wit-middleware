fe7d210c191b5b4f072c0609f3d50176da585999
Gaël <gael@hungryup.co>
2017-11-27 22:17:29 +0100

    ♻️ Refactor to fresh new API

--- index.js
+++ index.js
@@ -1,45 +1,30 @@
-const AbstractRouter = require('ticelli-bot/router');
-const merge = require('lodash.merge');
+const AbstractRouter = require('ticelli-bot');
 
 const { Wit } = require('node-wit');
 
 module.exports = class WitAiRouter extends AbstractRouter {
-  constructor(...params) {
-    super(...params);
-    this.witInstance = new Wit({ accessToken: this.config.access_token })
+  constructor(config) {
+    super({}, config);
+    this.witInstance = new Wit({ accessToken: this.config.access_token });
   }
 
-  async run(req) {
-    if (req.body.event && req.body.event.text) {
-      req.wit = await this.witInstance.message(req.body.event.text);
-      Object.defineProperty(req, 'intent', {
-        get() {
-          const { entities } = req.wit;
-          return Object.keys(entities).reduce((o, name) => {
-            const info = entities[name].reduce(merge);
-            if (info.type !== 'value') {
-              o[name] = info;
-            }
-            return o;
-          }, {});
+  async run(train) {
+    const { event } = train.request.body;
+    if (event.text) {
+      const { entities } = await this.witInstance.message(event.text);
+
+      train.intents = Object.keys(entities).reduce((o, name) => {
+        o[name] = entities[name].map(({ value }) => value);
+        return o;
+      }, {});
+
+      train.entities = Object.keys(entities).reduce((o, name) => {
+        const info = entities[name];
+        if (info.map(v => ['value', 'interval'].includes(v.type)).reduce((a, b) => a || b, false)) {
+          o[name] = info;
         }
-      });
-      Object.defineProperty(req, 'entities', {
-        get() {
-          const { entities } = req.wit;
-          return Object.keys(entities).reduce((o, name) => {
-            const info = entities[name];
-            if (info.map(v => ['value', 'interval'].includes(v.type)).reduce((a, b) => a || b, false)) {
-              o[name] = info;
-            }
-            return o;
-          }, {});
-        }
-      });
+        return o;
+      }, {});
     }
-  }
-
-  use(fn) {
-    throw new Error('You cannot use something on WitAi router, excepted to be used only as middleware')
   }
 };
